d64c6a775e9da5101ea0858562a5391c6bc8df7e
Ren Urot <ren.ensombl@gmail.com>
2026-07-19 06:26:42 +0800

    Remove PageHeader banner from Buy Crypto page

--- src/app/buy-crypto/page.tsx
+++ src/app/buy-crypto/page.tsx
@@ -1,16 +1,9 @@
-import PageHeader from "@/components/PageHeader";
 import BuyCryptoView from "@/components/buy-crypto/BuyCryptoView";
 
 export default function BuyCryptoPage() {
   return (
-    <>
-      <PageHeader
-        title="Buy crypto in a few steps"
-        description="Bitcoin, Ethereum, Solana, and more popular crypto."
-      />
-      <section className="bg-[#f2f2f4] px-9 pb-16">
-        <BuyCryptoView />
-      </section>
-    </>
+    <section className="bg-[#f2f2f4] px-9 pt-16 pb-16">
+      <BuyCryptoView />
+    </section>
   );
 }
